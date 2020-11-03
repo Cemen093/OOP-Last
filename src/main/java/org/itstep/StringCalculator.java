0d4ce6b62dcf4e8215bf07d2d71445fa9905a5bf
Cemen093 <CemenP93@gmail.com>
2020-11-03 16:16:38 +0200

    5 kata is ready

--- src/main/java/org/itstep/StringCalculator.java
+++ src/main/java/org/itstep/StringCalculator.java
@@ -4,10 +4,56 @@ public class StringCalculator {
     public static int add(String str) {
         if (str == null || "".equals(str)){return 0;}
 
-        String[] strNums = str.split(",");
+        String regex = ",|\\n";
+        String regexUser = "";
+        if (str.length() > 2 && str.substring(0,2).equals("//")){
+            int index = str.indexOf("\n");
+            regexUser = str.substring(2, index).trim();
+            regex += "|" + regexUser;
+            str = str.substring(index + 1);
+
+            if (regexUser.equals("*")){
+                regex = regex.substring(0, regex.length() - 2) + "[^0123456789]";
+            }
+
+            {
+                boolean check = false;
+                for (int i = 0; i < regexUser.length(); i++) {
+                    int number = (int) regexUser.charAt(i);
+                    if (!(number <= (int) '9' && number >= (int) '0')) {
+                        check = true;
+                    }
+                }
+                if (!check){
+                    System.out.print("throw SpliterFormatException");
+                    return 0;
+                }
+            }
+
+            if (regexUser.length() == 1 && (int)regexUser.charAt(0) <= (int)'9' &&
+                    (int)regexUser.charAt(0) >= (int)'1'){
+                System.out.print("throw SpliterFormatException");
+                return 0;
+            }
+        }
+
+        String[] strNums = str.split(regex);
+
+        for (int i = 0; i < strNums.length; i++) {
+            strNums[i] = strNums[i].trim();
+            for (int j = 0; j < strNums[i].length(); j++) {
+                if (!(strNums[i].charAt(j) >= '0' && strNums[i].charAt(j) <= '9')){
+                    System.out.print("throw SpliterFormatException");
+                    return 0;
+                }
+            }
+        }
+
         int sum = 0;
         for (String tmp: strNums) {
-            sum += Integer.parseInt(tmp);
+            if (Integer.parseInt(tmp) < 1001) {
+                sum += Integer.parseInt(tmp);
+            }
         }
         return sum;
     }
--- src/test/java/org/itstep/StringCalculatorTest.java
+++ src/test/java/org/itstep/StringCalculatorTest.java
@@ -3,6 +3,9 @@ package org.itstep;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class StringCalculatorTest {
 
     @Test
@@ -13,4 +16,49 @@ public class StringCalculatorTest {
         Assert.assertEquals(3, StringCalculator.add("1,2"));
         Assert.assertEquals(4, StringCalculator.add("4,"));
     }
+
+    @Test
+    public void kata02() {
+        Assert.assertEquals(6, StringCalculator.add("1,2,3"));
+        Assert.assertEquals(4, StringCalculator.add("1,1,1,1"));
+        Assert.assertEquals(3, StringCalculator.add(" 1,2 "));
+        Assert.assertEquals(66, StringCalculator.add("11,22,33"));
+    }
+
+    @Test
+    public void kata03(){
+        Assert.assertEquals(6, StringCalculator.add("1 \n2,3"));
+        Assert.assertEquals(15, StringCalculator.add("4\n5\n6"));
+        Assert.assertEquals(77, StringCalculator.add("77\n"));
+    }
+
+    @Test
+    public void kata04(){
+        Assert.assertEquals(1, StringCalculator.add("1, 1001"));
+        Assert.assertEquals(2, StringCalculator.add("1002 \n 2"));
+        Assert.assertEquals(1001, StringCalculator.add("1, 1000"));
+        Assert.assertEquals(2000, StringCalculator.add("1000, 1000"));
+        Assert.assertEquals(0, StringCalculator.add("1001\n1001"));
+    }
+
+    @Test
+    public void kata05(){
+        Assert.assertEquals(3, StringCalculator.add("//;\n1; 2"));
+        Assert.assertEquals(5, StringCalculator.add("//* \n2,3"));
+        Assert.assertEquals(7, StringCalculator.add("//#\n3# 4"));
+
+
+        ByteArrayOutputStream bufferedOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bufferedOutputStream));
+        StringCalculator.add("//1\n1 1 1");
+        Assert.assertEquals("throw SpliterFormatException", bufferedOutputStream.toString());
+        bufferedOutputStream.reset();
+        StringCalculator.add("//;\n1#2");
+        Assert.assertEquals("throw SpliterFormatException", bufferedOutputStream.toString());
+        System.setOut(null);
+
+    }
+
+    /*
+        Assert.assertEquals(, StringCalculator.add());*/
 }
